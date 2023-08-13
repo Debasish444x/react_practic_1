d47e7841c8b442f8a0b0a71f0440bb877232da59
Anwasa <anwasasil0019@gmail.com>
2023-08-12 20:54:00 -0700

    AddProduct.js modified

--- src/Components/products/AddProduct.js
+++ src/Components/products/AddProduct.js
@@ -1,7 +1,129 @@
-import React from 'react'
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import axios from 'axios'
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom'
 
-export const AddProduct = () => {
+export default function AddProduct() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+  // let validationError = {};
+  const [validationError={},setValidationError] = useState({})
+
+  const createProduct = async (e) => {
+    console.log();
+    e.preventDefault();
+
+    const formData = new FormData()
+
+    formData.append('name', name)
+    formData.append('price', price)
+    const token = localStorage.getItem("token");
+  
+    axios
+    .request({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method: "POST",
+      url: `http://localhost/newApp/api/registerProduct`,
+      data: formData,
+    })
+    .then((response) => {
+      console.log(response.data);
+      if(response.data.status===422){
+        console.log("422 error")
+        // setValidationError(response.data.error)
+        // {
+        //   error: response.data.error,
+        // }
+      }
+      if(response.status===201){
+        Swal.fire({
+          icon:"success",
+          text:response.data.message
+        })
+        navigate("/dashboard")
+      }
+      else{
+        Swal.fire({
+          text:response.data.message,
+          icon:"error"
+        })
+      }
+   
+    }).catch(({response})=>{
+      console.log(response);
+        Swal.fire({
+          text:response.data.message,
+          icon:"error"
+        })
+      
+    })
+  }
+
   return (
-    <div>AddProduct</div>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-12 col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Create Product</h4>
+              <hr />
+              <div className="form-wrapper">
+                {
+                  Object.keys(validationError).length > 0 && (
+                    <div className="row">
+                      <div className="col-12">
+                        <div className="alert alert-danger">
+                          <ul className="mb-0">
+                            {
+                              Object.entries(validationError).map(([key, value])=>(
+                                <li key={key}>{value}</li>   
+                              ))
+                            }
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+                <Form onSubmit={createProduct}>
+                  <Row> 
+                      <Col>
+                        <Form.Group controlId="Name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" value={name} onChange={(event)=>{
+                              setName(event.target.value)
+                            }}/>
+                        </Form.Group>
+                      </Col>  
+                  </Row>
+                  <Row className="my-3">
+                      <Col>
+                        <Form.Group controlId="Description">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control as="textarea" rows={3} value={price} onChange={(event)=>{
+                              setPrice(event.target.value)
+                            }}/>
+                        </Form.Group>
+                      </Col>
+                  </Row>
+                 
+                  <Button variant="primary" className="mt-2" size="lg" block="block" type="submit">
+                    Save
+                  </Button>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
